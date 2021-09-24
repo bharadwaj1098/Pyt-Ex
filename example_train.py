354f17f5e8a8f14c74c1ceedd309ae365698271d
bharadwaj1098 <bharadwaj.reddy98@gmail.com>
2021-09-24 19:40:26 -0400

    del exaple_train.py

--- example_train.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from pyt_ex import neural_net as ann
-
-with open('config/Input.yaml') as File:
-        dic = yaml.load(File, Loader=yaml.FullLoader)
-
-for i in dic:
-    dnn = ann.dynamic_nn(dic[i])
-    dnn.show() 
